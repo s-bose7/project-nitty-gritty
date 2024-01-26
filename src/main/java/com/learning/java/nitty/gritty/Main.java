fd0b2875714043ef7cac3a161b4b99d52eb24ab5
Sudip Bose <sbose007ime.work@gmail.com>
2024-01-27 03:31:54 +0530

    java language structure

---
+++ src/main/java/com/learning/java/nitty/gritty/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.learning.java.nitty.gritty;
+
+/**
+ * @author BOSE SUDIP
+ *
+ */
+
+public class Main {
+
+	/**
+	 * Topics: 
+	 * - 
+	 */
+	public static void main(String[] args) {
+		System.out.println("start");
+	}
+
+}
---
+++ src/main/java/com/learning/java/nitty/gritty/collections/package-info.java
@@ -0,0 +1,1 @@
+package com.learning.java.nitty.gritty.collections;
